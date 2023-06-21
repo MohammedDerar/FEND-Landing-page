6f7a1b3a74a7b9dd7ff83a1dc4bdb4302c8ad3e4
MohammedDerar <92974205+MohammedDerar@users.noreply.github.com>
2023-06-21 22:48:33 +0400

    landing page project

---
+++ Js/app.js
@@ -0,0 +1,65 @@
+// cerate the nav menue based on page sections
+function createNavMenu() {
+    const sectionsList = document.querySelectorAll('section');
+    const menu = document.querySelector('.menu');
+    let i = 1;
+    for (const section of sectionsList) {
+        const navItem = document.createElement('li');
+        const link = section.getAttribute('id');
+        menu.appendChild(navItem);
+        navItem.innerHTML = `<a class='${link}' href='#${link}'>Section ${i}</a>`;
+        i += 1;
+    }
+}
+
+// to check if the section is totally or partially in viewport
+function isSectionInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    const viewPortHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const visibleHeight = Math.min(rect.bottom, viewPortHeight) - Math.max(rect.top, 0);
+    const elementHeight = rect.bottom - rect.top;
+    const isElementCompletelyInViewport = rect.top >= 0 && rect.bottom <= viewPortHeight;
+    const isElementPartiallyInViewport = rect.top < viewPortHeight && rect.bottom > 0;
+    const isViewportCoveredByElement = rect.top <= 0 && rect.bottom >= viewPortHeight;
+    return (
+        isElementCompletelyInViewport ||
+        (isElementPartiallyInViewport && visibleHeight >= elementHeight * 0.55) ||
+        isViewportCoveredByElement
+    );
+}
+
+/* add active-section class to the section in viewport and remove 
+   if it goes out
+*/
+function addOrRemoveActiveClass(){
+    const sectionsList = document.querySelectorAll('section');
+    for (const section of sectionsList) {
+        section.classList.toggle('active-section', isSectionInViewport(section));
+    }
+}
+
+/* add active-link class to the nav element which is
+   related to the viewed section
+*/
+function checkActiveLink() {
+    const sections = document.querySelectorAll('section');
+    const links = document.querySelectorAll('li a');
+    for(const section of sections) {
+        if (isSectionInViewport(section)) {
+            links.forEach(link => {
+                if(link.classList.contains(section.id)) {
+                    link.classList.add('active-link');
+                } else {
+                    link.classList.remove('active-link');
+                }
+            })
+        }        
+                
+
+       
+    }
+}
+
+createNavMenu();
+document.addEventListener('scroll', addOrRemoveActiveClass);
+document.addEventListener('scroll', checkActiveLink);
---
+++ README.md
@@ -0,0 +1,23 @@
+# Intro
+## FEND LANDING-PAGE PROJECT
+
+This project is a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page. And through the nav menu user can scroll to the required section and it will be highlighted as an active section.
+
+### Getting started
+- User can run index.html on the browser then start navigating between section.
+
+### Running localy
+- The Landing page can be run directly on the browser.
+
+### Testing
+ - Not applicable for this project.
+
+### Project Structure
+```
+css
+- styles.css	
+index.html
+js
+- app.js
+README.md
+```
